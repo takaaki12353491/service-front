82f99a6c9e34b2a3fb4216875495887cc58ef0f5
Takaaki Inoue <takaaki@MacBook-Pro.local>
2020-10-02 07:21:20 +0900

    Modify Header with material-ui

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {AppBar,Toolbar,Typography,Button,IconButton} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-interface HeaderProps {
-  UserName: string
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
-const Header: React.FC<HeaderProps> = (
-  props:HeaderProps
-): JSX.Element => {
-  return(
-    <header className="header">
-
-    </header>
-  )
-}
-export default Header
