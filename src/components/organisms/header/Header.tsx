86aeb1580b0400cd450aa97bcac50a81de7b5dcd
Takaaki Inoue <takaaki@MacBook-Pro.local>
2020-10-02 09:09:15 +0900

    Create Home page

--- src/components/organisms/header/Header.tsx
+++ src/components/organisms/header/Header.tsx
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export default function ButtonAppBar() {
+export default function Header() {
   const classes = useStyles();
 
   return (
---
+++ src/components/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import Header from '../../organisms/header/Header'
+
+interface Props {
+}
+
+const Home: React.FC<Props> = (props) => {
+  return(
+    <Header></Header>
+  )
+}
+export default Home
