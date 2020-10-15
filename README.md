9aa33b857c6a45bb3566ffab09ce3fb7ff72384b
takaaki12353491 <takaaki12353491@gmail.com>
2020-10-15 09:11:48 +0900

    Create PrivateRoute

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ https://github.com/MichalSzorad/react-social-login-buttons
 https://github.com/mdbootstrap/react-bootstrap-with-material-design
 https://github.com/lipis/bootstrap-social
 https://github.com/react-bootstrap/react-bootstrap
+
+# route
+https://medium.com/@thanhbinh.tran93/private-route-public-route-and-restricted-route-with-react-router-d50b27c15f5e
---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Route, RouteProps, Redirect } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+
+const PrivateRoute = (props: RouteProps) => {
+  const value = useContext(AuthContext);
+  const Component = value.isAuthenticated ? <Route {...props}/> : <Redirect to='/login'/>
+  return (
+    Component
+  )
+}
+export default PrivateRoute;
