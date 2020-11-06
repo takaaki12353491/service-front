63b62b95f400acc71925fd36d93cefc7e2e40e27
takaaki12353491 <takaaki12353491@gmail.com>
2020-11-06 14:23:47 +0900

    Add community page

--- src/components/pages/communities/Community.tsx
+++ src/components/pages/communities/Community.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import Header from '../../organisms/Header'
+import { useParams } from 'react-router-dom'
+import { Axios } from '../../../modules/Axios'
+import { Type } from '../../../modules/Type'
 
 const Community = () => {
+  const { id } = useParams()
+  const [ community, setCommunity ] = React.useState<Type.Community>({
+    ID: '', Owner: { ID: '', Name: '', Email: ''}, Name: '',
+  })
+  Axios.Default
+    .get(`${Axios.URL.COMMUNITIES}/${id}`)
+    .then((res: Axios.Response<Type.Community>) => {
+      res.status === 200 && setCommunity(res.data)
+    })
+    .catch()
   return (
-    <Header/>
+    <div>
+      <p>{community.Name}</p>
+    </div>
   )
 }
 export default Community
--- src/routes/Communities.tsx
+++ src/routes/Communities.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route, Link, useParams, useRouteMatch} from "react-router-dom"
 import PrivateRoute from './PrivateRoute'
 import NewCommunity from '../components/pages/communities/NewCommunity'
+import Community from '../components/pages/communities/Community'
 
 const Communities = () => {
   const match = useRouteMatch()
@@ -9,7 +10,7 @@ const Communities = () => {
   return (
     <Switch>
       <PrivateRoute exact path={`${match.url}/new`} component={NewCommunity}/>
-      <Route exact path={`${match.url}/:id`}/>
+      <Route exact path={`${match.url}/:id`} component={Community}/>
     </Switch>
   )
 }
