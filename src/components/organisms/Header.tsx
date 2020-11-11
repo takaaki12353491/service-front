78707f8258ceb9e699bf73f99f97b0cd3212c77f
takaaki12353491 <takaaki12353491@gmail.com>
2020-11-11 09:11:37 +0900

    Modify Header

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,59 +3,64 @@ import styled, { StyledFunction } from 'styled-components'
 import { State } from '../../modules/State'
 import URL from '../../consts/URL'
 import Name from '../../consts/Name'
-import Input, { InputProps } from '../atoms/Input'
-import Select, { SelectProps } from '../atoms/Select'
-import Link, { LinkProps } from '../atoms/Link'
+import Input from '../atoms/Input'
+import Select from '../atoms/Select'
+import Link from '../atoms/Link'
 
-const Header = () => {
-  const HomeLink = styled(Link)`
-    color: white;
-    text-decoration: none;
-    &:hover: {
-      color: gray;
-    }
-  `
-  const PatternSelect: SelectProps = {
-    name: Name.PATTERN,
-    options: [
-      { value: 'name', text: 'name' },
-      { value: 'tag', text: 'tag' },
-    ],
-  }
-  const TargetSelect: SelectProps = {
-    name: Name.TARGET,
-    options: [
-      { value: 'account', text: 'account' },
-      { value: 'community', text: 'community' },
-      { value: 'project', text: 'project' },
-    ]
-  }
-  const SearchInput: InputProps = {
-    name: Name.SEARCH
-  }
-
-  const Container = styled.header`
-    display: flex;
-    position: sticky;
-    width: 100%;
-    height: 60px;
-    top: 0;
-    align-items: center;
-    justify-content: space-evenly;
-    background-color: #000000;
-  `
-
-  const Search = styled.div`
-  `
+const Header: React.FC = () => {
   return (
     <Container>
       <HomeLink href='/' text='Service'/>
       <Search>
-        <Select {...PatternSelect}/>
-        <Select {...TargetSelect}/>
-        <Input {...SearchInput} />
+        <PatternSelect 
+          name={Name.PATTERN}
+          options={[
+            { value: 'name', text: 'name' },
+            { value: 'tag', text: 'tag' },
+          ]}
+        />
+        <TargetSelect 
+          name={Name.TARGET}
+          options={[
+            { value: 'account', text: 'account' },
+            { value: 'community', text: 'community' },
+            { value: 'project', text: 'project' },
+          ]}  
+        />
+        <Input name={Name.SEARCH}/>
       </Search>
     </Container>
   )
 }
 export default Header
+
+const Container = styled.header`
+  display: flex;
+  position: sticky;
+  width: 100%;
+  height: 60px;
+  top: 0;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: black;
+`
+
+const HomeLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover: {
+    color: gray;
+  }
+`
+
+const Search = styled.div`
+`
+
+const PatternSelect = styled(Select)`
+`
+
+const TargetSelect = styled(Select)`
+`
+
+const SearchBar = styled(Input)`
+`
