e7a214d656b01bb32062b66184a168cbd74fa5e9
takaaki12353491 <takaaki12353491@gmail.com>
2020-11-12 03:29:10 +0900

    Refactoring

--- src/components/pages/communities/NewCommunity.tsx
+++ src/components/pages/communities/NewCommunity.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-import { useHistory } from 'react-router-dom'
 import { useForm } from "react-hook-form"
 import Form, { FormContext, FormProps } from '../../organisms/Form'
 
-const NewCommunity = () => {
-  const classes = useStyles()
+const NewCommunity: React.FC = () => {
   type FormData = {
     name: string
     description: string
@@ -14,47 +11,38 @@ const NewCommunity = () => {
   const onSubmit = handleSubmit(value => {
 
   })
-  const formValue: FormProps = {
-    submit: onSubmit,
-    button: { text: 'Submit' },
-    items: [
-      {
-        item: {
-          id: 'header',
-        }
-      },
-      {
-        item: {
-          id: 'icon'
-        }
-      },
-      {
-        item: {
-          name: 'name', label: 'name',
-          inputRef: register(),
-        }
-      },
-      {
-        item: {
-          name: 'description', label: 'description',
-          multiline: true, rows: 4,
-          inputRef: register(),
-        }
-      },
-    ]
-  }
   return (
-    <FormContext.Provider value={formValue}>
+    <FormContext.Provider value={{
+      submit: onSubmit,
+      button: { text: 'Submit' },
+      items: [
+        {
+          item: {
+            id: 'header',
+          }
+        },
+        {
+          item: {
+            id: 'icon'
+          }
+        },
+        {
+          item: {
+            name: 'name', label: 'name',
+            inputRef: register(),
+          }
+        },
+        {
+          item: {
+            name: 'description', label: 'description',
+            multiline: true, rows: 4,
+            inputRef: register(),
+          }
+        },
+      ]
+    }}>
       <Form/>
     </FormContext.Provider>
   )
 }
 export default NewCommunity
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    header: {
-      width: '100%',
-    },
-  }),
-)
