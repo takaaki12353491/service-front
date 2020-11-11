4aed3ae98f711efda569cd310cbc44e2e31ac195
takaaki12353491 <takaaki12353491@gmail.com>
2020-11-12 02:33:41 +0900

    Create Footer

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+export default Footer
+
+const Container = styled.footer`
+  position: fixed;
+  width: 100%;
+  height: 60px;
+  bottom: 0;
+  text-align: center;
+  z-index: 1000;
+  background-color: black;
+`
