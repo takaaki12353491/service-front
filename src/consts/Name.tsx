84a12f7da4041cf9b3247ce6737972f57b44ef81
takaaki12353491 <takaaki12353491@gmail.com>
2020-11-20 16:30:02 +0900

    Add DESCRIPTION to Name

--- src/consts/Name.tsx
+++ src/consts/Name.tsx
@@ -1,10 +1,11 @@
 export namespace Name {
+  export const PATTERN = 'pattern'
+  export const TARGET = 'target'
+  export const SEARCH = 'search'
   export const NAME = 'name'
   export const EMAIL = 'email'
   export const IDENTITY = 'identity'
   export const PASSWORD = 'password'
-  export const PATTERN = 'pattern'
-  export const TARGET = 'target'
-  export const SEARCH = 'search'
+  export const DESCRIPTION = 'description'
 }
 export default Name
