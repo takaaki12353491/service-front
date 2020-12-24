188f1a5be25970a3689a26cf1c64db17a484b459
takaaki12353491 <takaaki12353491@gmail.com>
2020-12-24 16:43:30 +0900

    Modify Input

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,30 +1,14 @@
 import React from 'react'
 
-export interface InputProps {
-  name?: string
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string
   type?: 'text' | 'number' | 'password' | 'url'
-  placeholder?: string
-  defaultValue?: string
-  maxLength?: number
-  minLength?: number
-  onChange?: any
-  inputRef?: any
-  className?: string
+  inputRef?: string | ((instance: HTMLInputElement | null) => void) | React.RefObject<HTMLInputElement> | null
 }
 
 export const Input: React.FC<InputProps> = (props) => {
   return (
-    <input 
-      name={props.name}
-      type={props.type}
-      placeholder={props.placeholder}
-      defaultValue={props.defaultValue}
-      maxLength={props.maxLength}
-      minLength={props.minLength}
-      onChange={props.onChange}
-      ref={props.inputRef}
-      className={props.className}
-    />
+    <input {...props} ref={props.inputRef}/>
   )
 }
 export default Input
